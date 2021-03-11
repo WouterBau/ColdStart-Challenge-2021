5c39505134585eebffb3044482e18adef50c8631
Wouter Bauchart <wouter.bauchart@outlook.com>
2021-03-11 01:23:29 +0100

    Finished the tedious task of DB query for catalog retrieval

--- api/catalog-get/index.js
+++ api/catalog-get/index.js
@@ -1,11 +1,16 @@
 const data = require('../shared/catalog-data');
 
+//https://devblogs.microsoft.com/azure-sql/promises-node-tedious-azure-sql-oh-my/
 module.exports = function (context, req) {
-  try {
-    const items = data.getCatalog();
-    context.res.status(200).send(items);
-  } catch (error) {
-    context.res.status(500).send(error);
-  }
-  context.done();
+  data.getCatalog()
+  .then(catalog => {
+    context.res.body = catalog;
+    context.res.status = 200;
+    context.done();
+  })
+  .catch(err => {
+    context.log.error(err);
+    context.res.status = 500;
+    context.done();
+  });
 };
--- api/shared/catalog-data.js
+++ api/shared/catalog-data.js
@@ -1,26 +1,25 @@
-//const fs = require('fs').promises;
 const config = require('../shared/dbconfig');
 const { Connection, Request } = require("tedious");
 
-var catalog = { icecreams : [] };
+const getCatalog = () => new Promise((resolve, reject) => {
+  
+  var catalog = [];
 
-const executeSql = () => new Promise((resolve, reject) => {
-  const request = new Request('SELECT * FROM [dbo].[Icecreams];', (err, rowCount, rows) => {
+  const request = new Request('SELECT * FROM [dbo].[Icecreams];', (err, rowCount) => {
     if (err) {
       console.log("sql err");
       console.error(err.message);
       reject(err);
     }
-    console.log(`${rowCount} row(s) returned`);
-    resolve();
+    resolve(catalog);
   });
+
   request.on("row", columns => {
     var row = {};
     columns.forEach(column => {
       row[column.metadata.colName] = column.value;
     });
-    catalog.icecreams.push(row);
-    console.log(catalog);
+    catalog.push(row);
   });
 
   const connection = new Connection(config.config);
@@ -35,21 +34,5 @@ const executeSql = () => new Promise((resolve, reject) => {
 
   connection.connect();
 });
-
-function getCatalog() {
-  var catalog = { icecreams : [] };
-  
-  executeSql()
-  .then()
-  .catch(err => {throw err;});
-
-  console.log(catalog);
-  return catalog;
-
-  /*console.log('using static data.');
-  var stringData = await fs.readFile('./shared/catalog.json', 'utf8');
-  const data = JSON.parse(stringData);  
-  return data.icecreams;*/
-}
 
 module.exports = { getCatalog };
