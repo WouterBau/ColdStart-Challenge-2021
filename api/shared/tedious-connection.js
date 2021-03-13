b342fcd94d54f2db0bc317bdc9b0677b48c4513c
Wouter Bauchart <wouter.bauchart@outlook.com>
2021-03-13 01:26:09 +0100

    Added personalizer config and removed need of dbconfig

--- api/shared/tedious-connection.js
+++ api/shared/tedious-connection.js
@@ -1,8 +1,8 @@
-const config = require('../shared/dbconfig');
+const config = require('../shared/config');
 const { Connection } = require("tedious");
 
 function getConnection() {
-    const connection = new Connection(config.config);
+    const connection = new Connection(config.dbConfig);
     return connection;
 };
 
